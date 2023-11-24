5c9efee4fa51a699635b44e616fe8d2c288aafbb
Carlos <chsl200293@gmail.com>
2023-11-24 00:11:17 -0300

    [GITHUB] - add pojo

---
+++ src/main/java/com/chslcompany/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,36 @@
+package com.chslcompany.picpaysimplificado.domain.transaction;
+
+import com.chslcompany.picpaysimplificado.domain.user.Users;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private Users sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private Users receiver;
+
+    private LocalDateTime timestamp;
+}
